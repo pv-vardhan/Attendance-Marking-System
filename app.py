fb91aa0eb85a7bb74337856687b35b69e0af6145
pv-vardhan <vishnuvardhan0811@gmail.com>
2024-06-01 21:09:43 +0530

    change

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 import subprocess
+
 app = Flask(__name__)
 
 
@@ -10,8 +11,13 @@ def index():
 
 @app.route('/run_scripts')
 def run_scripts():
+    # Execute main.py
     main_result = subprocess.run(['python3', 'main.py'], capture_output=True, text=True)
+
+    # Execute Program.java
+
     java_result = subprocess.run(['python3', 'Checkbox.py'], capture_output=True, text=True)
+
     # Combine results
     combined_output = main_result.stdout + '\n' + java_result.stdout
 
